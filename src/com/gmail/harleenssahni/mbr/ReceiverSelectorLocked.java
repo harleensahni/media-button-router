ab0ee4ecae6271f162da3f558d5c4b8444a9f099
Harleen Sahni <harleenssahni@gmail.com>
2011-04-16 19:31:46 -0400

    Don't show options menu when dialog is showing over lock screen.

--- src/com/gmail/harleenssahni/mbr/ReceiverSelectorLocked.java
+++ src/com/gmail/harleenssahni/mbr/ReceiverSelectorLocked.java
@@ -1,10 +1,17 @@
 package com.gmail.harleenssahni.mbr;
 
+import android.view.Menu;
+
 /**
  * Allows us to display on lock screen.
  * 
  * @author harleenssahni@gmail.com
  */
 public class ReceiverSelectorLocked extends ReceiverSelector {
-    // Intentionally blank
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // No context menu, we're showing above the lock screen. Can't get to
+        // our preferences screen anyway.
+        return false;
+    }
 }
